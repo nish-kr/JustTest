f8208d051942324f1085fc0e9f5d1c70cd305e94
Nishant Kumar <nishant.kr10@gmail.com>
2015-01-20 23:33:14 +0530

    This is just a test.

---
+++ a.cpp
@@ -0,0 +1,36 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <cstdio>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <map>
+#include <queue>
+#include <cstring>
+#define iofast ios_base::sync_with_stdio(false)
+#define ull unsigned long long
+#define ll long long
+#define ui unsigned int
+#define db double
+#define pb push_back
+#define mp make_pair
+#define INF 1000000000
+#define inf 0x7fffffff
+#define MOD 1000000007
+
+using namespace std;
+
+typedef pair<int,int> pii;
+typedef list<int> li;
+typedef vector<int> vi;
+typedef vector< vector<int> > vvi;
+typedef vector< vector< pii > > vvp;
+
+int main(){
+	iofast;
+	stringstream ss;
+	cout<<"Hello world!!"<<endl;
+}
+
